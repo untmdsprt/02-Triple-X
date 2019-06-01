d230c0bc90623bacd144dad0f8ac98a0e980721f
Jenny A <untmdsprt@gmail.com>
2019-06-01 16:49:40 -0500

    S2L22 - const & Assigning Values

--- triplex.cpp
+++ triplex.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "You are going to be robbing a bank";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct codes to continue...";
+    std::cout << std::endl;
+
+    const int a = 4;
+    const int b = 3;
+    const int c = 5;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
